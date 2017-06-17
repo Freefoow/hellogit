d9d92c3226b946a3fb9d9a522767a4bae8786814
FFO <fredfoudol@gmail.com>
2017-06-17 22:51:08 +0200

    [Lignes supprimées]

--- HelloGit/src/com/java/hellogit/HelloGit.java
+++ HelloGit/src/com/java/hellogit/HelloGit.java
@@ -3,16 +3,12 @@ package com.java.hellogit;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		
 		String hello = "hello ";
 		SaySomething(hello);
-
 	}
 	
 	public static void SaySomething(String word){
-		
 		System.out.println(word);
-		
 	}
 
 }
