62aac0785bbcc626cf2fba3bfa82f7d3499a748b
FFO <fredfoudol@gmail.com>
2017-06-17 22:29:27 +0200

    First commit project HelloGit

---
+++ HelloGit/src/com/java/hellogit/HelloGit.java
@@ -0,0 +1,18 @@
+package com.java.hellogit;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		String hello = "hello ";
+		SaySomething(hello);
+
+	}
+	
+	public static void SaySomething(String word){
+		
+		System.out.println(word);
+		
+	}
+
+}
