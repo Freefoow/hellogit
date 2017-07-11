c22a8e9175bd74ee6b606afab1c2180bc90886d0
FFO <fredfoudol@gmail.com>
2017-07-11 22:11:29 +0200

    Branche2 hello2

--- HelloGit/src/com/java/hellogit/HelloGit.java
+++ HelloGit/src/com/java/hellogit/HelloGit.java
@@ -3,7 +3,7 @@ package com.java.hellogit;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		String hello = "hello 1 ";
+		String hello = "hello 2 ";
 		SaySomething(hello); 
 	}
 	
