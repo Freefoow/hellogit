e9b12512b81368b45c39213a6dd4b4acd9f5b16d
FFO <fredfoudol@gmail.com>
2017-07-26 23:24:53 +0200

    add something

--- HelloGit/src/com/java/hellogit/HelloGit.java
+++ HelloGit/src/com/java/hellogit/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		String hello = "hello ";
 		SaySomething(hello);
+		System.out.println("add something");
 	}
 	
 	public static void SaySomething(String word){
